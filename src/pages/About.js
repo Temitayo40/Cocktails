f24f96424ed787fd528749bffeaa196603bb23e6
Adebowale <adebowaletemitayo40@gmail.com>
2022-12-13 08:28:55 +0100

    cocktail webpage using react.js

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, iusto eos?
+        Molestiae, iusto modi! Quidem animi dignissimos et qui sit deleniti
+        quisquam aspernatur facere quo possimus explicabo eaque accusantium
+        maiores, distinctio tempore error? Temporibus nam mollitia repudiandae a
+        culpa pariatur!
+      </p>
+    </section>
+  );
+};
+
+export default About;
